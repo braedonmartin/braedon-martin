6242f74681e10cc7e22dbddd95674154dce9e952
braedonmartin <braedonmartin25@gmail.com>
2022-07-18 13:47:43 -0400

    doing some routing testing

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className='app'>
       <Routes>
-        <Route exact path='/' element={<Layout />}>
+        <Route path='braedonmartin.com/' element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
