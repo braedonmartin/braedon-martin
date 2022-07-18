3158cddccb99192d51c784b915ed849858e929a6
braedonmartin <braedonmartin25@gmail.com>
2022-07-18 16:38:17 -0400

    fixing client side routing stuff

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ function App() {
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
           <Route path="portfolio" element={<Portfolio />} />
-          <Route path="*" element={<Home />} />
         </Route>
       </Routes>
     </div>
--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -29,7 +29,6 @@ function Layout() {
                     </div>
                 </div>
             <Sidebar />
-
         </div>
     )
 }
